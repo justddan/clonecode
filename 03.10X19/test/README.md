ef13aa23d117f4de73401d3e44ca108782f8f1ae
justddan <100176542+justddan@users.noreply.github.com>
2022-05-11 14:48:18 +0900

    1019

--- 03.10X19/test/README.md
+++ 03.10X19/test/README.md
@@ -1,4 +1,3 @@
-
 # tailwindcss cli 실행
 
 npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
--- 03.10X19/test/tailwind.config.js
+++ 03.10X19/test/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        personal: "#af7832",
+      },
+      animation: {
+        "personal-animation": "line 3s linear infinite",
+      },
+      keyframes: {
+        line: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
